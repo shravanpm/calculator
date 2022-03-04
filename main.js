92af8e68abd3d7f1e07e630e84496a2380ee5062
shravanpm <97494889+shravanpm@users.noreply.github.com>
2022-03-04 07:10:10 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const {add} = require("./add.js");
+const { divide } = require("./divide.js");
+const { multiply } = require("./multiply.js");
+const { subtract } = require("./subtract.js");
+
+
+console.log(subtract(5,3));
+
+console.log(add(2,3));
+
+console.log(multiply(2,3));
+
+console.log(divide(6,2));
